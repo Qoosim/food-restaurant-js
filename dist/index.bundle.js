d0097c967b1ce94ac71d5798213fb51c01186f02
dasileker <dasileker@gmail.com>
2022-05-09 16:59:21 +0100

    add codes

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -8,27 +8,30 @@
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _asset_food_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./asset/food.jpg */ "./src/asset/food.jpg");
+/* harmony import */ var _asset_pizza_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./asset/pizza.jpg */ "./src/asset/pizza.jpg");
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'index.css'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
 // import Img from './asset/food.jpg';
+
 
 
 const color = document.querySelector('body');
 
 const banner = document.createElement('div');
+banner.innerHTML = `<img class="food" src=${_asset_pizza_jpg__WEBPACK_IMPORTED_MODULE_0__} style = 'width =10px;'>`;
 color.appendChild(banner);
-banner.src = _asset_food_jpg__WEBPACK_IMPORTED_MODULE_0__;
+
 
 
 
 /***/ }),
 
-/***/ "./src/asset/food.jpg":
-/*!****************************!*\
-  !*** ./src/asset/food.jpg ***!
-  \****************************/
+/***/ "./src/asset/pizza.jpg":
+/*!*****************************!*\
+  !*** ./src/asset/pizza.jpg ***!
+  \*****************************/
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-module.exports = __webpack_require__.p + "fa99c409e3a554a9f5da.jpg";
+module.exports = __webpack_require__.p + "a31bc6c7b728bb26a665.jpg";
 
 /***/ })
 
@@ -38,4 +41,4 @@ module.exports = __webpack_require__.p + "fa99c409e3a554a9f5da.jpg";
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
 /******/ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQUE7QUFDa0M7O0FBRWxDOztBQUVBO0FBQ0E7QUFDQSxhQUFhLDRDQUFHIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZm9vZC1yZXN0YXVyYW50Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIGltcG9ydCBJbWcgZnJvbSAnLi9hc3NldC9mb29kLmpwZyc7XG5pbXBvcnQgSW1nIGZyb20gJy4vYXNzZXQvZm9vZC5qcGcnXG5cbmNvbnN0IGNvbG9yID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignYm9keScpO1xuXG5jb25zdCBiYW5uZXIgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdkaXYnKTtcbmNvbG9yLmFwcGVuZENoaWxkKGJhbm5lcik7XG5iYW5uZXIuc3JjID0gSW1nO1xuXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7OztBQUFBO0FBQ21DO0FBQ1A7O0FBRTVCOztBQUVBO0FBQ0EsNENBQTRDLDZDQUFHLEVBQUUscUJBQXFCO0FBQ3RFIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZm9vZC1yZXN0YXVyYW50Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIGltcG9ydCBJbWcgZnJvbSAnLi9hc3NldC9mb29kLmpwZyc7XG5pbXBvcnQgSW1nIGZyb20gJy4vYXNzZXQvcGl6emEuanBnJ1xuaW1wb3J0IGNzcyBmcm9tICdpbmRleC5jc3MnO1xuXG5jb25zdCBjb2xvciA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJ2JvZHknKTtcblxuY29uc3QgYmFubmVyID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnZGl2Jyk7XG5iYW5uZXIuaW5uZXJIVE1MID0gYDxpbWcgY2xhc3M9XCJmb29kXCIgc3JjPSR7SW1nfSBzdHlsZSA9ICd3aWR0aCA9MTBweDsnPmA7XG5jb2xvci5hcHBlbmRDaGlsZChiYW5uZXIpO1xuXG5cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // import Img from './asset/food.jpg';
-import Img from './asset/food.jpg'
+import Img from './asset/pizza.jpg'
+// import css from './css';
 
 const color = document.querySelector('body');
 
 const banner = document.createElement('div');
-banner.innerHTML = `<img src=${Img}>`;
+banner.innerHTML = ``;
 color.appendChild(banner);
 
 
