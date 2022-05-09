5ec57122fdeb00382adf5a1c4e42c77186536b5e
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-05-09 17:38:15 +0100

    Adjust webpack.configure to use css file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-// import Img from './asset/food.jpg';
 import Img from './asset/pizza.jpg'
-// import css from './css';
+import './style.css';
 
 const color = document.querySelector('body');
 
 const banner = document.createElement('div');
-banner.innerHTML = ``;
+banner.id = 'main-container';
+banner.style.width = '50%';
+banner.style.height = '50%';
+banner.style.backgroundColor = '#f15025';
 color.appendChild(banner);
 
 
